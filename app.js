9029fc4ed7055d5e37d144bc894ba6f75e2d015b
dmartz86 <dmartz86@gmail.com>
2014-02-20 17:39:01 -0500

    Making better the script
    Randomizing email.
    Adding order to the test. (Namespace)
    Adding set interval.
    Opening and closing the db connection.

--- app.js
+++ app.js
@@ -1,47 +1,86 @@
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
+/**
+ * The Authentication controller.
+ */
+var StressJS = function() {
+};
 
-	function randomString(size) {
-		var s = "";
-		var keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-*=!#$%&";
-		for (var i = 0; i < size; i++){
-			s += keys.charAt(Math.floor(Math.random() * keys.length));
-		}
-		return s;
+/**
+ * Create random string.
+ * @param size, the lenght of the string
+ */
+StressJS.randomString = function(size) {
+	var s = "";
+	var keys = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+-*=!#$%&";
+	for (var i = 0; i < size; i++) {
+		s += keys.charAt(Math.floor(Math.random() * keys.length));
 	}
+	return s;
+};
 
-	function save(id) {
-		var title = randomString(20) + new Date() + '_' + Math.floor((Math.random() * 100));
+/**
+ * Opens and close the db.
+ * Save a post and save the user email
+ */
+StressJS.save = function() {
+	MongoClient.connect('mongodb://localhost:27017/test', function(err, db) {
 
+		//Selecting the post collection
 		var posts = db.collection("posts");
 
-		var permalink = title
+		var title = StressJS.randomString(20) + new Date() + '_' + Math.floor((Math.random() * 100));
+		var permalink = title;
+		var user = {
+			fname : StressJS.randomString(8),
+			lname : StressJS.randomString(8),
+			domain : '@' + StressJS.randomString(10) + '.' + StressJS.randomString(5) + '.' + StressJS.randomString(5)
+		};
+
+		user.email = user.fname + "." + user.lname + user.domain;
 
 		// Build a new post
 		var post = {
 			"title" : "MNA || " + title,
-			"author" : randomString(8)+"."+randomString(8) + '@mailsample.com',
+			"author" : user.email,
 			"body" : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
 			"permalink" : permalink,
-			"tags" : [randomString(5), randomString(2), randomString(6)],
-			"comments" : [randomString(4) + '2' + randomString(4)],
+			"tags" : [StressJS.randomString(5), StressJS.randomString(2), StressJS.randomString(6)],
+			"comments" : [StressJS.randomString(4) + '2' + StressJS.randomString(4)],
 			"date" : new Date().getDate()
-		}
-		posts.insert(post, function(err, result) {"use strict";
+		};
 
+		posts.insert(post, function(err, result) {
 			if (err) {
-				console.log("ERR " + id);
+				console.log("Post ERROR ");
+				db.close();
 			} else {
-				console.log("OK " + id);
+				console.log("Post OK ");
+				//Selecting the post collection
+				var users = db.collection("users");
+				users.insert(user, function(err, result) {
+					if (err) {
+						console.log("Post ERROR ");
+						db.close();
+					} else {
+						console.log("User OK ");
+						db.close();
+					}
+				});
 			}
 		});
-	}
 
-	var max = 2000000;
-	for (var i = 0; i < max; i++) {
-		setTimeout(function(){
-			save(i);	
-		},100);
-	};
-});
+	});
+};
+
+/**
+ * Here we go!.
+ */
+StressJS.run = function() {
+	setInterval(function() {
+			StressJS.save();
+		}, 100);
+};
+
+//Exports the object
+exports.StressJS = StressJS;
